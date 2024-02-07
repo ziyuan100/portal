f587fd6cb190a16d578a82345b39b6152c3e03f3
zyshi's mac <ziyuan.shi@yahoo.com>
2024-02-07 23:01:02 +0800

    misc. changes, sync

--- app/screens/Explore.js
+++ app/screens/Explore.js
@@ -1,9 +1,39 @@
 import { View, Text } from "react-native"
+import innerStyles from "../styles/InnerStyles";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { FIRESTORE_DB } from "../../firebaseConfig";
 
 const Explore = () => {
+    const [activities, setActivities] = useState([]);
+
+    const getActivities = async () => {
+        try {
+            const q = query(collection(FIRESTORE_DB, "activities"));
+            const querySnapshot = await getDocs(q);
+            const acts = [];
+            querySnapshot.forEach(doc => {
+                acts.push(doc.data());
+            })
+            setActivities(acts);
+        } catch (e) {
+            console.error(e);
+        }
+        
+    }
+
+    useEffect(() => {
+        getActivities();
+    }, [])
+
     return (
         <View style={{flex: 1}}>
-            <Text style={{color: "#fff"}}>Testing</Text>
+            {activities.forEach(activity => {
+                console.log("jsx", activity);
+                return(
+                    <Text style={{color:"#fff"}}>{activity.name}</Text>
+                )  
+            })}
         </View>
     )
 }
